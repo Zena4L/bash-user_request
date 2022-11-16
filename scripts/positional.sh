2c434a6fcc427e7395e48d795c2d48f8ec258f7b
Clement Bogyah <Zena@Clements-MacBook-Pro.local>
2022-11-16 10:49:45 +0000

    file added

---
+++ scripts/positional.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# This script runs for positonal parameters
+# Positonal parameter uses regular parameter expansions
+
+echo "My name is $1"
+echo "My Home directory is $2"
+echo "My fav color is $3"
+exit 0
+
+
