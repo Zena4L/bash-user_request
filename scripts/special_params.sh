75677f3966f81956cd595fd711a0df51c50e5c3f
Clement Bogyah <Zena@Clements-MacBook-Pro.local>
2022-11-16 11:02:22 +0000

    file addded

---
+++ scripts/special_params.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Special paramters
+# $# and $0
+
+# $# give out the total number of parameters
+# #0 ecpends to the name of the script
+
+
+if [[ $# -ne 2]]; then
+    echo "You didn't enter exactly 2 parameters"
+    echo "Usage: $0 <file1> <file2>"
+    exit 1
+fi
+exit 0
+
+
